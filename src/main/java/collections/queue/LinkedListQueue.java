e02162b10f7079238689040deb5f6cb7a58fe089
iga <iga.smarz@gmail.com>
2018-02-01 09:02:27 +0100

    some improvments to LinkedListQueue

--- src/main/java/collections/queue/LinkedListQueue.java
+++ src/main/java/collections/queue/LinkedListQueue.java
@@ -1,5 +1,6 @@
 package collections.queue;
 
+//to be continued...
 public class LinkedListQueue<E> implements Queue<E>{
 
     private Link head;
@@ -12,18 +13,35 @@ public class LinkedListQueue<E> implements Queue<E>{
 
     @Override
     public void add(E element) {
-        if(size==0){
-            head = new Link();
-            head.element = element;
-        }
+      if(size==0){
+        head = new Link();
+        head.element = element;
+        head.previous = null;
+        head.next = null;
+      }else if(size==1){
+        tail = new Link();
+        tail.element = element;
+        tail.previous = head;
+        tail.next = null;
+        head.next = tail;
+      }else{
+        Link oldTail = tail;
+        tail = new Link();
+        tail.previous = oldTail;
+        tail.element = element;
+        tail.next = null;
+      }
 
         size++;
     }
 
     @Override
     public E remove() {
-        size--;
-        return head.element;
+      E removedElement = head.element;
+      head = head.next;
+      head.previous = null;
+      size--;
+      return removedElement;
     }
 
     @Override
@@ -31,9 +49,24 @@ public class LinkedListQueue<E> implements Queue<E>{
         return this.size;
     }
 
+    @Override
+    public String toString(){
+      StringBuilder builder = new StringBuilder();
+      Link currentLink = head;
+      while(currentLink != null){
+        builder.append(currentLink.element.toString()).append(", ");
+        currentLink = currentLink.next;
+      }
+      return builder.toString();
+    }
+
     private class Link{
         Link previous;
         E element;
         Link next;
+
+        public String toString(){
+          return element.toString();
+        }
     }
 }
--- src/test/java/collections/queue/LinkedListQueueTest.java
+++ src/test/java/collections/queue/LinkedListQueueTest.java
@@ -6,57 +6,93 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LinkedListQueueTest {
 
-    @Test
-    public void whenCreatingNewQueue_expectSizeEquals0(){
-        Queue<String> queue = new LinkedListQueue<>();
-        assertThat(queue.size()).isEqualTo(0);
-    }
+  @Test
+  public void whenCreatingNewQueue_expectSizeEquals0() {
+    Queue <String> queue = new LinkedListQueue <>();
+    assertThat(queue.size()).isEqualTo(0);
+  }
 
-    @Test
-    public void whenAddingOneElementToEmptyQueue_expectedSizeEquals1(){
-        Queue<String> queue = new LinkedListQueue<>();
-        queue.add("item1");
-        assertThat(queue.size()).isEqualTo(1);
-    }
+  @Test
+  public void whenAddingOneElementToEmptyQueue_expectedSizeEquals1() {
+    Queue <String> queue = new LinkedListQueue <>();
+    queue.add("item1");
+    assertThat(queue.size()).isEqualTo(1);
+  }
 
-    @Test
-    public void whenAddingElementToEmptyQueueAndRemovingElement_expectedElementsToBeEqual(){
-        Queue<String> queue = new LinkedListQueue<>();
-        String element = "item1";
-        queue.add(element);
-        assertThat(element).isEqualTo(queue.remove());
-    }
+  @Test
+  public void whenAddingElementToEmptyQueueAndRemovingElement_expectedElementsToBeEqual() {
+    Queue <String> queue = new LinkedListQueue <>();
+    String element = "item1";
+    queue.add(element);
+    assertThat(element).isEqualTo(queue.remove());
+  }
 
-    @Test
-    public void whenAddingTwoElementsToEmptyQueue_expectedSizeEquals2(){
-        Queue<String> queue = new LinkedListQueue<>();
-        queue.add("item1");
-        queue.add("item2");
-        assertThat(queue.size()).isEqualTo(2);
-    }
+  @Test
+  public void whenAddingTwoElementsToEmptyQueue_expectedSizeEquals2() {
+    Queue <String> queue = new LinkedListQueue <>();
+    queue.add("item1");
+    queue.add("item2");
+    assertThat(queue.size()).isEqualTo(2);
+  }
 
-    @Test
-    public void whenAddingTwoElementsToEmptyQueueAndRemovingOneElement_expectedRemovedElementToBeEqualToTheFirstOne(){
-        Queue<String> queue = new LinkedListQueue<>();
-        String firstElement = "item1";
-        queue.add(firstElement);
-        String secondElement = "item2";
-        queue.add(secondElement);
-        String firstRemoved = queue.remove();
-        String secondRemoved = queue.remove();
-        assertThat(firstElement).isEqualTo(firstRemoved);
-    }
+  @Test
+  public void whenAddingTwoElementsToEmptyQueueAndRemovingOneElement_expectedRemovedElementToBeEqualToTheFirstOne() {
+    Queue <String> queue = new LinkedListQueue <>();
+    String firstElement = "item1";
+    queue.add(firstElement);
+    String secondElement = "item2";
+    queue.add(secondElement);
+    String firstRemoved = queue.remove();
+    String secondRemoved = queue.remove();
+    assertThat(firstElement).isEqualTo(firstRemoved);
+  }
 
-    @Test
-    public void whenAddingTwoElementsToEmptyQueueAndRemovingTwoElements_expectedSecondRemovedElementToBeEqualToTheSecondOneAdded(){
-        Queue<String> queue = new LinkedListQueue<>();
-        String firstElement = "item1";
-        queue.add(firstElement);
-        String secondElement = "item2";
-        queue.add(secondElement);
-        String firstRemoved = queue.remove();
-        String secondRemoved = queue.remove();
-        assertThat(secondElement).isEqualTo(secondRemoved);
-    }
+  @Test
+  public void whenAddingTwoElementsToEmptyQueueAndRemovingTwoElements_expectedSecondRemovedElementToBeEqualToTheSecondOneAdded() {
+    Queue <String> queue = new LinkedListQueue <>();
+    String firstElement = "item1";
+    queue.add(firstElement);
+    String secondElement = "item2";
+    queue.add(secondElement);
+    String firstRemoved = queue.remove();
+    String secondRemoved = queue.remove();
+    assertThat(secondElement).isEqualTo(secondRemoved);
+  }
+
+  @Test
+  public void whenAdding3ElementsToEmptyQueueAndRemovingOneElement_expectedRemovedElementToBeEqualToTheFirstAdded() {
+    Queue <String> queue = new LinkedListQueue <>();
+    String firstElement = "item1";
+    queue.add(firstElement);
+    queue.add("item2");
+    queue.add("item3");
+    String firstRemoved = queue.remove();
+    assertThat(firstElement).isEqualTo(firstRemoved);
+  }
+
+  @Test
+  public void whenAdding3ElementsToEmptyQueueAndRemoving2Elements_expectedSecondRemovedElementToBeEqualToTheSecondOneAdded() {
+    Queue <String> queue = new LinkedListQueue <>();
+    queue.add("item1");
+    String item2 = "item2";
+    queue.add(item2);
+    queue.add("item3");
+    queue.remove();
+    String secondRemoved = queue.remove();
+    assertThat(item2).isEqualTo(secondRemoved);
+  }
+
+  @Test
+  public void whenAdding3ElementsToEmptyQueueAndRemoving3Elements_expectedLastRemovedElementToBeEqualToTheLastOneAdded() {
+    Queue <String> queue = new LinkedListQueue <>();
+    queue.add("item1");
+    queue.add("item2");
+    String item3 = "item3";
+    queue.add(item3);
+    queue.remove();
+    queue.remove();
+    String thirdRemoved = queue.remove();
+    assertThat(item3).isEqualTo(thirdRemoved);
+  }
 
 }
